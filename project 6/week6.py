02a7679f892685037f9a07eddb518922c48eb5d3
c-redmond <redmondcharles999@gmail.com>
2025-10-02 12:51:46 -0500

    Create week6.py

---
+++ project 6/week6.py
@@ -0,0 +1,29 @@
+#question 5
+def hailstone_seq(n):
+    output_list = []
+    while n != 1:
+        if n % 2 == 0:
+            n/=2
+        else:
+           n = (n*3)+1
+        output_list.append(n)
+    return output_list
+
+#question 9
+
+def count(list_of_cards):
+    points = 0
+    for card in list_of_cards:
+        temp_card = str(card)
+        if temp_card in ["2",'3','4','5','6']:
+            points += 1
+        elif temp_card in ['10','j','q','k','a']:
+            points -= 1
+    return points
+
+deck_1 = [5,9,10,3,'j','a',4,8,5]
+deck_2 = ['a','a','k','q','q','j']
+
+print(count(deck_2))
+
+
