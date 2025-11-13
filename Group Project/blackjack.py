4f51192eb9c6b6549386dc954ecfab410734402a
c-redmond <redmondcharles999@gmail.com>
2025-11-13 12:55:15 -0600

    Update blackjack.py

--- Group Project/blackjack.py
+++ Group Project/blackjack.py
@@ -1,10 +1,10 @@
 
 #properties of the players
 class Player:
-    def __init__(self, hand, money, points):
+    def __init__(self, hand = [], money = 100, points = 0):
         self.hand = []
-        self.money = 100
-        self.points = 0
+        self.money = money
+        self.points = points
     
     def get_points(self):
         return self.points
@@ -18,8 +18,9 @@ class Player:
     def set_hand(self, hand):
         self.hand = hand
     def set_money(self, money):
-        self.money = 0
-player_1 = Player({}, 0, 0)
+        self.money = money
+player_1 = Player()
+computer_player = Player([], 999999999999999, 0)
 
 # creating the deck of cards and stroing their values, then appending the keys to a list that can be shuffled and dealt from  
 ace_value = 1
@@ -29,26 +30,66 @@ deck_list = []
 for key in deck_dict.keys():
     deck_list.append(key)
 
-
-
-
 #deal cards
 import random
+def deal(Player, deck_list):
+    dealt_card = random.choice(deck_list)
+    Player.hand.append(dealt_card)
+    deck_list.remove(dealt_card)
+    return Player.get_hand()
 
-dealt_card = random.choice(deck_list)
-player_1.hand.append(dealt_card)
-for card in deck_list:
-    if card == dealt_card:
-        deck_list.remove(dealt_card)
-print(player_1.get_hand())
-#some code here that returns 2 keys and values from the deck dictionary
 #calculate points in hand
 
-if (player_1.get_points() + 11) > 21:
+'''if (player_1.get_points() + 11) > 21:
     ace_value = 1
 else:
     ace_value = 11
 
 for key, value in deck_dict.items():
-    if key in player_1.get_hand():
-        
+    if key in player_1.get_hand():'''
+    
+
+#once dealing function, point calc function, and player class logic are done:
+#player places a bet, deal 2 cards to the player and the computer, calculate their points
+#if the player wins, they win a prize equal to what they bet, if they get a 21, they win 1.5 * their initial bet, and if they tie, they get their money back without winning any new money
+#initital bet, deal, bet again, hit or stay (until they stay), calculate points, declare winner, distribute money
+
+print(f'money: ${player_1.get_money()}')
+init_bet = int(input('place your bet'))
+deal(player_1, deck_list)
+deal(player_1, deck_list)
+deal(computer_player, deck_list)
+deal(computer_player, deck_list)
+print(f'your hand: {player_1.get_hand()}')
+print(f"dealer's hand: {computer_player.get_hand()}")
+double_down_choice = input('would you like to double down? (y/n)')
+if double_down_choice == 'y':
+    double_down = init_bet
+else:
+    double_down = 0
+
+#hit/stay logic
+hit_stay = input('would you like to hit or stay?')
+if hit_stay == 'hit':
+    deal(player_1, deck_list)
+elif hit_stay == 'stay':
+    pass
+else:
+    print('invalid input')
+
+#dealer logic. the dealer will always hit if their point total is 16 or lower
+
+#determine the winner, and distribute money. still needs logic for 21s
+'''player_points = point_calc(player_1)
+comp_points = point_calc(computer_player)
+if player_points > comp_points:
+    player_1.set_money(player_1.get_money() + init_bet + double_down)
+    print('you defeated!')
+    print(f'money: ${player_1.get_money()}')
+elif player_points < comp_points:
+    player_1.set_money(player_1.get_money() - init_bet - double_down)
+    print('you lost, better luck next time :()')
+    print(f'money: ${player_1.get_money()}')
+else:
+    print("it's a tie")
+    print(f'money: ${player_1.get_money()}')'''
